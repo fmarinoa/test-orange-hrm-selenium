1d4704ff3db337161d614382b1fa613ccddb3e2a
Francoedson <franco.edson.18014@gmail.com>
2024-10-08 10:18:35 -0500

    New locators in Login Page

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static hooks.Hooks.getDriver;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(xpath = "//input[@name='username']")
+    protected WebElement inputUsername;
+
+    @FindBy(xpath = "//input[@name='password']")
+    protected WebElement inputPassword;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    protected WebElement btnLogin;
+
+    // Inicializa los elementos
+    public LoginPage() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+}
--- src/test/java/pages/Pages.java
+++ src/test/java/pages/Pages.java
@@ -5,4 +5,8 @@ public class Pages {
     public BasePage basePage() {
         return new BasePage();
     }
+
+    public LoginPage loginPage() {
+        return new LoginPage();
+    }
 }
