89cbb3da4881562387e4903b513f567c64288271
Francoedson <franco.edson.18014@gmail.com>
2024-10-13 12:15:07 -0500

    Created Employee class using lombok

---
+++ src/test/java/models/Employee.java
@@ -0,0 +1,17 @@
+package models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Employee {
+
+    public String Id;
+    public String firstName;
+    public String middleName;
+    public String lastName;
+
+    public Employee() {
+    }
+}
